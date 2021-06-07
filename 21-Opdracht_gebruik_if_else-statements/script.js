4f82b6d7a90e5b0e46afcdb197a90661f6e03af2
Romy <80348985+ROOMH@users.noreply.github.com>
2021-06-07 11:52:16 +0200

    Add files via upload

---
+++ 21-Opdracht_gebruik_if_else-statements/script.js
@@ -0,0 +1,43 @@
+const age = 18;
+console.log(age);
+
+if (age == 18) {
+
+    document.write("Je MAG naar binnen");
+
+} else if (age > 18) {
+
+    document.write("Je MAG naar binnen");
+
+} else if (age <18) {
+
+    document.write("Je mag NIET naar binnen");
+   
+}
+
+
+const isFemale = true;
+console.log(isFemale);
+
+if (isFemale = true) {
+
+    document.write(Vrouw);
+
+} else if ( isFemale = false) {
+
+    document.write(Man);
+
+}
+
+const driverStatus = bob;
+console.log(driveStatus);
+
+if (driveStatus = bob) {
+
+    write.document("Goed");
+
+} else if (driveStatusc= dronken) {
+
+    document.write("Fout");
+}
+
